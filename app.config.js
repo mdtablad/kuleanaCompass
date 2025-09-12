32c3990b4e9860dbcb3d1d8a5911e05aa45b6043
Altronxs <kylextron101098@gmail.com>
2025-09-11 22:37:11 -1000

    updated from Expo 53 to 54

--- app.config.js
+++ app.config.js
@@ -8,5 +8,10 @@ export default {
     extra: {
       googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
     },
+    plugins: [
+      "expo-font",
+      "expo-router",
+      "expo-web-browser"
+    ],
   },
 };
