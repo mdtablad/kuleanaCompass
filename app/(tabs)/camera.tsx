1c08c2c9ab0b1e66da7fbcf58d656908783c735d
Altronxs <kylextron101098@gmail.com>
2025-10-01 11:57:17 -1000

    made camera nicer and added the feature of gemini identifying what the thing is in the picture

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -5,12 +5,16 @@ import {
   useCameraPermissions,
 } from "expo-camera";
 
-import { useRef, useState } from "react";
-import { Button, Pressable, StyleSheet, Text, View } from "react-native";
+import { useRef, useState, useEffect } from "react";
+import { Button, Pressable, StyleSheet, Text, View, ActivityIndicator, ScrollView, TouchableOpacity, KeyboardAvoidingView, Platform } from "react-native";
 import { Image } from "expo-image";
-import AntDesign from "@expo/vector-icons/AntDesign";
-import Feather from "@expo/vector-icons/Feather";
+import Ionicons from '@expo/vector-icons/Ionicons';
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { useFonts } from '@expo-google-fonts/roboto/useFonts';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import * as FileSystem from 'expo-file-system/legacy';
+import { getGeminiTextWithImage } from '@/lib/geminiClients';
+import "@/app/globals.css";
 
 export default function App() {
   const [permission, requestPermission] = useCameraPermissions();
@@ -19,6 +23,15 @@ export default function App() {
   const [mode, setMode] = useState<CameraMode>("picture");
   const [facing, setFacing] = useState<CameraType>("back");
   const [recording, setRecording] = useState(false);
+  const [prompt, setPrompt] = useState('');
+  const [response, setResponse] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [base, setBase] = useState<string | null>(null);
+  const [imageUri, setImageUri] = useState<string | null>(null);
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [shouldProcessImage, setShouldProcessImage] = useState(false);
+
+
 
   if (!permission) {
     return null;
@@ -37,9 +50,19 @@ export default function App() {
 
   const takePicture = async () => {
     const photo = await ref.current?.takePictureAsync();
+    //console.log(photo)
     if (photo?.uri) {
-      setUri(photo.uri)
-      console.log(photo.uri);
+      setUri(photo.uri);
+
+      const base64Image = await FileSystem.readAsStringAsync(photo.uri, {
+        encoding: 'base64',
+      });
+
+      setPrompt("What is this picture showing? (limit one word, No need to explain just identify)");
+      setLoading(true);
+      const result = await getGeminiTextWithImage(prompt, base64Image);
+      setResponse(result);
+      setLoading(false);
     };
   };
 
@@ -68,9 +91,8 @@ export default function App() {
         <Image
           source={{ uri }}
           contentFit="contain"
-          style={{ width: 300, aspectRatio: 1 }}
+          style={{ width: 1000, aspectRatio: 1 }}
         />
-        <Button onPress={() => setUri(null)} title="Take another picture" />
       </View>
     );
   };
@@ -107,7 +129,7 @@ export default function App() {
               </View>
             )}
           </Pressable>
-          <Pressable onPress={toggleFacing}>
+          <Pressable onPress={toggleFacing} style={styles.facingToggle}>
             <FontAwesome6 name="rotate-left" size={32} color="white" />
           </Pressable>
         </View>
@@ -117,7 +139,28 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {uri ? renderPicture(uri) : renderCamera()}
+      {uri ? (
+        <>
+          {renderPicture(uri)}
+          {loading ? (
+            <ActivityIndicator color="white" style={{ marginTop: 20 }} />
+          ) : (
+            
+              <Text className='color-white mt-20 text-[20px] font-roboto-bold z-10 absolute top-0 text-center w-[60vw]'>
+                {response}
+              </Text>
+            
+          )}
+          <Pressable onPress={() => setUri(null)} 
+            className='z-10 absolute top-0 left-0 mt-20 ml-4'  
+          >
+            {/* <Text
+              className="color-white mt-20 text-[20px] font-roboto-bold"
+            ></Text> */}
+            <Ionicons name="arrow-back-circle" color="white" size={28} />
+          </Pressable>
+        </>
+      ) : renderCamera()}
       
     </View>
   );
@@ -134,18 +177,17 @@ const styles = StyleSheet.create({
   camera: StyleSheet.absoluteFillObject,
   shutterContainer: {
     position: "absolute",
-    bottom: 44,
+    bottom: 80,
     left: 0,
     width: "100%",
     alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "space-between",
     paddingHorizontal: 30,
   },
   shutterBtn: {
     backgroundColor: "transparent",
     borderWidth: 5,
     borderColor: "white",
+    bottom: 0,
     width: 85,
     height: 85,
     borderRadius: 45,
@@ -156,5 +198,11 @@ const styles = StyleSheet.create({
     width: 70,
     height: 70,
     borderRadius: 50,
+  },
+    facingToggle: {
+    position: "absolute",
+    bottom: 25,
+    right: 0,
+    marginRight: 50
   },
 });
