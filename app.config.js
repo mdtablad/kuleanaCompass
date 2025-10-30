59d32ecaed29e0273c5c38d47e84342ec15f2c2f
Kyle Alexander <108177142+Altronxs@users.noreply.github.com>
2025-10-29 23:14:56 -1000

    Add files via upload

---
+++ app.config.js
@@ -0,0 +1,19 @@
+export default {
+  expo: {
+    ios: {
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+        geminiApiKey: process.env.GEMINI_API_KEY,
+      },
+    },
+    extra: {
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      geminiApiKey: process.env.GEMINI_API_KEY,
+    },
+    plugins: [
+      "expo-font",
+      "expo-router",
+      "expo-web-browser"
+    ],
+  },
+};
